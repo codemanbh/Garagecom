cf89aefc7ef07a116907b07e42ee39ae0dfdf5d9
alqassmiii <qassimhassan9@gmail.com>
2025-05-13 08:53:59 +0300

    changed the ngrok url

--- lib/helpers/apiHelper.dart
+++ lib/helpers/apiHelper.dart
@@ -12,8 +12,7 @@ class ApiHelper {
   // late Dio dio;
   // static String mainDomain = 'https://55ac-2001-1a40-100f-4700-9822-a5d9-b897-d978.ngrok-free.app/';
   static String mainDomain =
-      'https://assuring-fox-conversely.ngrok-free.app/'; // fixed ngrok
-  // static String mainDomain = 'https://10.0.2.2:5294/'; // fixed ngrok
+      'https://98f1-193-188-13-253.ngrok-free.app/'; // fixed ngrok
 
   static Future<Dio> Client() async {
     final prefs = await SharedPreferences.getInstance();
